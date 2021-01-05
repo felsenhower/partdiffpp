03ddb7fadc916c03be7f70e219d979eff9ff297d
Ruben Felgenhauer <4felgenh@informatik.uni-hamburg.de>
2021-01-05 15:53:50 +0100

    Restore benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,12 +1,10 @@
 
 #!/usr/bin/env bash
 
-n=4
-
 for i in {1..10} ; do
   for p in 'pde' 'pde++' ; do
     echo -n "$p"' '
-    (/usr/bin/time -f '%U' $p/partdiff 1 1 1024 2 2 1 >/dev/null) 2>&1
+    (/usr/bin/time -f '%U' $p/partdiff 1 1 1024 2 2 60 >/dev/null) 2>&1
    done
 done | awk 'BEGIN {
   pde = 0.0;
