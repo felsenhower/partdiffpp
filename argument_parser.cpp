7fe40bde22320c4b52870a55fd260099b9d63dbe
Ruben Felgenhauer <ruben.felgenhauer@posteo.de>
2025-11-26 14:17:00 +0100

    Move calculation_options definition to dedicated header

--- argument_parser.cpp
+++ argument_parser.cpp
@@ -1,4 +1,5 @@
 #include "argument_parser.hpp"
+#include "calculation_options.hpp"
 
 #include "partdiff.h"
 
--- partdiff.h
+++ partdiff.h
@@ -25,21 +25,6 @@ namespace partdiff {
     return static_cast<U>(v);
   }
 
-  namespace askparams {
-
-    struct calculation_options {
-      enum class calculation_method : uint64_t { gauss_seidel = 1, jacobi = 2 };
-      enum class perturbation_function : uint64_t { f0 = 1, fpisin = 2 };
-      enum class termination_condition : uint64_t { accuracy = 1, iterations = 2 };
-      uint64_t number;
-      uint64_t interlines;
-      calculation_method method;
-      perturbation_function pert_func;
-      termination_condition termination;
-      uint64_t term_iteration;
-      double term_accuracy;
-    };
-
-  } // namespace askparams
+  namespace askparams {} // namespace askparams
 
 } // namespace partdiff
