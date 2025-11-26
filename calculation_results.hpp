f2d52a3577122484d3e67c8fbaa756ee64ebfc9e
Ruben Felgenhauer <ruben.felgenhauer@posteo.de>
2025-11-26 13:58:15 +0100

    Simplify calculation_results

---
+++ calculation_results.hpp
@@ -0,0 +1,14 @@
+#include <chrono>
+
+namespace partdiff {
+
+  struct calculation_results {
+    using time_point = std::chrono::time_point<std::chrono::high_resolution_clock>;
+    int m;
+    uint64_t stat_iteration;
+    double stat_accuracy;
+    time_point start_time;
+    time_point end_time;
+  };
+
+} // namespace partdiff
